a5132ccd1fb31768144a30a3f16458d094177917
Bhryan <bhryan.perpetuo@praxent.com>
2025-12-19 16:06:21 -0300

    Remove pupetter

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * Puppeteer configuration file
- * Ensures Chrome is found in the correct cache location on Render
- */
-module.exports = {
-  cacheDirectory: process.env.PUPPETEER_CACHE_DIR || join(__dirname, '.cache', 'puppeteer'),
-};
